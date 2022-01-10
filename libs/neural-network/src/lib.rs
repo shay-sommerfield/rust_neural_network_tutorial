9f39d0f77ac0f78208f74ba04b2f928a17ba0c6c
Shay <shaysommerfield@gmail.com>
2022-01-09 16:47:22 -0800

    functional NN library that compiles

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,20 +1,28 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
 
 
 pub struct Network {
     layers: Vec<Layer>,
 }
 impl Network {
-    // a non-mutable input can be passed to a function that requires a mutable argument
-    pub fn propagate(&self,mut inputs: Vec<f32>) -> Vec<f32>{
+    fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
 
         // for layer in &self.layers {
         //     inputs = layer.propagate(inputs);
         // }
+        // inputs
 
-        // The following is the same as the above for loop
-        self.layers.iter().fold(inputs, |inputs, layer| layer.propagate(inputs))
+        self.layers
+        .iter()
+        .fold(inputs, |inputs, layer| layer.propagate(inputs))
 
-        inputs
     }
 }
 
@@ -22,8 +30,21 @@ struct Layer {
     neurons: Vec<Neuron>,
 }
 impl Layer {
-    fn propagate(&self,inputs: Vec<f32>) -> Vec<f32>{
-        todo!()
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        //let mut outputs = Vec::with_capacity(self.neurons.len());
+
+        // for neuron in &self.neurons {
+        //     let output = neuron.propagate(&inputs);
+        //     outputs.push(output);
+        // }
+
+        // outputs
+
+
+        self.neurons
+            .iter()  // creates an iterator that knows its length
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()  // creates a vector that's large enough
     }
 }
 
@@ -31,8 +52,22 @@ struct Neuron {
     bias: f32,
     weights: Vec<f32>,
 }
-impl Neruon { 
-    fn propagate(&self, inputs:Vec<f32>) -> f32 {
-        todo!()
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        let mut output = 0.0;
+        
+        for i in 0..inputs.len() {
+            output += inputs[i]*self.weights[i]
+        }
+
+        output += self.bias;
+
+        if output > 0.0 {
+            output
+        } else {
+            0.0
+        }
+        
     }
 }
