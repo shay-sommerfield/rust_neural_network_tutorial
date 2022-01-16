fed91ddc7cf87e3c93b6822ee6e40553e266af8f
Shay <shaysommerfield@gmail.com>
2022-01-15 17:23:54 -0800

    Fixed cargo test

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -173,8 +173,8 @@ mod tests{
             let mut rng = ChaCha8Rng::from_seed(Default::default());
             let neuron = Neuron::random(&mut rng,4);
 
-            assert_eq!(neuron.bias,0.0);
-            assert_eq!(neuron.weights,&[0.0,0.0,0.0,0.0]);
+            assert_eq!(neuron.bias,-0.6255188);
+            assert_eq!(neuron.weights,&[0.67383957, 0.8181262, 0.26284897, 0.5238807]);
     }
 
     }
