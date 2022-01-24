0758405997a9246772d1f978a9eec1d981f2d733
Shay <shaysommerfield@gmail.com>
2022-01-23 16:02:45 -0800

    refactored code again, roulette wheel testing

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -22,3 +22,114 @@ impl GeneticAlgorithm{
             .collect()
     }
 }
+
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
+use rand::RngCore;
+
+pub trait SelectionMethod {
+    fn select<'a, I>(
+       &self,
+       rng: &mut dyn RngCore,
+       population: &'a [I],
+    ) -> &'a I
+    where
+        I: Individual;
+}
+
+
+
+use rand::Rng;
+use rand::seq::SliceRandom;
+
+pub struct RouletteWheelSelection;
+
+impl RouletteWheelSelection {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(
+       &self,
+       rng: &mut dyn RngCore,
+       population: &'a [I],
+    ) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("got an empty population")
+    }    
+}
+
+#[cfg(test)]
+#[derive(Clone, Debug)]
+pub struct TestIndividual {
+    fitness: f32,
+}
+
+#[cfg(test)]
+impl TestIndividual {
+    pub fn new(fitness: f32) -> Self {
+        Self { fitness }
+    }
+}
+
+#[cfg(test)]
+impl Individual for TestIndividual {
+    fn fitness(&self) -> f32 {
+        self.fitness
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use std::iter::FromIterator;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test() {
+        let method = RouletteWheelSelection::new();
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+    
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+    
+        let mut actual_histogram = BTreeMap::new();
+    
+        //               there is nothing special about this thousand;
+        //          v--v a number as low as fifty might do the trick, too
+        for _ in 0..1000 {
+            let fitness = method
+                .select(&mut rng, &population)
+                .fitness() as i32;
+    
+            *actual_histogram
+                .entry(fitness)
+                .or_insert(0) += 1;
+        }
+    
+        let expected_histogram = BTreeMap::from_iter(vec![
+            // (fitness, how many times this fitness has been chosen)
+            (1, 98),
+            (2, 202),
+            (3, 278),
+            (4, 422),
+        ]);
+    
+        assert_eq!(actual_histogram, expected_histogram);
+    }
+}
